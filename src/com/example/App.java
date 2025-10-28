2ca376e048eff2b54792c57c6a192cc223830eb6
vinnu <vinod@vinods-MacBook-Air.local>
2025-10-28 23:57:10 +0530

    4

--- src/com/example/App.java
+++ src/com/example/App.java
@@ -1,11 +1,11 @@
-package main.java.com.example;
-
+package com.example;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from Java Maven CI/CD Demo!");
     }
 
- 
-    
+    public static String getMessage() {
+        return "Hello from Java Maven CI/CD Demo!";
+    }
 }
