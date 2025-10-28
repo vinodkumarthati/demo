6d694fd1f23a251d3bb742e01b722f4ff9f6285d
vinnu <vinod@vinods-MacBook-Air.local>
2025-10-28 23:18:29 +0530

    commit1

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package main.java.com.example;
+
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java Maven CI/CD Demo!");
+    }
+
+ 
+    
+}
